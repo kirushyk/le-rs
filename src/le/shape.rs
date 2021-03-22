7f8ec5c7a18fe34a90de40b699e8a8b7d6a18f3a
Kyrylo Polezhaiev <kirushyk@gmail.com>
2021-03-22 21:09:48 +0200

    shape: Use memory allocated at lower level

--- src/le/shape.rs
+++ src/le/shape.rs
@@ -2,7 +2,8 @@ use std::os::raw::c_void;
 
 #[link(name = "le")]
 extern "C" {
-    fn le_shape_new_from_data(num_dimensions: usize, sizes: *const usize) -> *const c_void;
+    fn le_shape_new_uninitialized(num_dimensions: usize) -> *mut c_void;
+    fn le_shape_set_size(shape: *mut c_void, dimension: usize, size: u32);
 }
 
 pub struct Shape {
@@ -10,8 +11,13 @@ pub struct Shape {
 }
 
 impl Shape {
-    pub fn new(num_dimensions: usize, sizes: &[usize]) -> Shape {
-        let shape = unsafe { le_shape_new_from_data(num_dimensions, sizes.as_ptr()) };
+    pub fn new(num_dimensions: usize, sizes: &[u32]) -> Shape {
+        let shape = unsafe { le_shape_new_uninitialized(num_dimensions) };
+        for (index, size) in sizes.iter().enumerate() {
+            unsafe {
+                le_shape_set_size(shape, index, *size);
+            }
+        }
         Shape { shape }
     }
 }
