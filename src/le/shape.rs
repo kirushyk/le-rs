f77d9187afab7ba68debd14b8453f374e0625323
Kyrylo Polezhaiev <kirushyk@gmail.com>
2021-03-23 03:24:51 +0200

    shape: Rename C struct

--- src/le/shape.rs
+++ src/le/shape.rs
@@ -3,21 +3,21 @@ use std::os::raw::c_void;
 #[link(name = "le")]
 extern "C" {
     fn le_shape_new_uninitialized(num_dimensions: usize) -> *mut c_void;
-    fn le_shape_set_size(shape: *mut c_void, dimension: usize, size: u32);
+    fn le_shape_set_size(c_shape: *mut c_void, dimension: usize, size: u32);
 }
 
 pub struct Shape {
-    pub shape: *const c_void
+    pub c_shape: *const c_void
 }
 
 impl Shape {
     pub fn new(num_dimensions: usize, sizes: &[u32]) -> Shape {
-        let shape = unsafe { le_shape_new_uninitialized(num_dimensions) };
+        let c_shape = unsafe { le_shape_new_uninitialized(num_dimensions) };
         for (index, size) in sizes.iter().enumerate() {
             unsafe {
-                le_shape_set_size(shape, index, *size);
+                le_shape_set_size(c_shape, index, *size);
             }
         }
-        Shape { shape }
+        Shape { c_shape }
     }
 }
